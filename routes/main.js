275f4dd284579b2fc0ee650ca554a305da15d276
M.Ihsan Fadilah <61552754+isanbayo@users.noreply.github.com>
2021-03-29 16:53:30 +0700

    mengerikan

--- routes/main.js
+++ routes/main.js
@@ -24,9 +24,9 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
+            namabot: 'sanbot',
+            namaowner: 'ihsan',
+            instagram: 'isanbayo',
             youtube : 'Gak Punya'
         }
     }
